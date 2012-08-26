c6b531e4d48a56a00db82d8b4dcc4c65a8829085
Christian Sommer <christian.sommer@gmx.ch>
2012-08-26 11:31:37 +0200

    Removed obsolete code blocks

--- cwsoft-postits/code/postits_functions.php
+++ cwsoft-postits/code/postits_functions.php
@@ -139,22 +139,3 @@ function getTemplateData()
 	// return template data
 	return $data;
 }
-
-function getPostitsVersion() {
-	// returns the actual installed AFE version (vX.Y.Z)
-	require_once(dirname(__FILE__) . '/../info.php');
-	return 'v' . $module_version;
-}
-
-function getReadmeUrl($afe_version) {
-	// returns the Url to the GitHub README for the installed AFE version
-	$url = 'https://github.com/cwsoft/wb-addon-file-editor/#readme';
-	
-	if (preg_match('#(v\d*\.\d*\.\d*)(.*)#i', $afe_version, $match)) {
-		// only stable versions (vX.Y.Z) are tagged at GitHub
-		if (! $match[2]) {
-			$url = 'https://github.com/cwsoft/wb-addon-file-editor/tree/' . $match[1] . '#readme';
-		}
-	}
-	return $url;
-}
